1e18cbe6da3f0544f3fdb4ee396bca1fe17abeb9
Steve Kuznetsov <skuznets@redhat.com>
2022-07-10 12:05:16 -0600

    danger-icons: fix up static mapping, missing extreme
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- src/components/AvalancheDangerIcon.tsx
+++ src/components/AvalancheDangerIcon.tsx
@@ -16,56 +16,66 @@ interface size {
 export const AvalancheDangerIcon: React.FunctionComponent<
   AvalancheDangerIconProps
 > = ({style, level}: AvalancheDangerIconProps) => {
-  const sizes: Record<DangerLevel, size> = {};
-  sizes[DangerLevel.GeneralInformation] = Image.resolveAssetSource(
-    require('@app/images/danger-icons/0.png'),
-  );
-  sizes[DangerLevel.None] = Image.resolveAssetSource(
-    require('@app/images/danger-icons/0.png'),
-  );
-  sizes[DangerLevel.Low] = Image.resolveAssetSource(
-    require('@app/images/danger-icons/1.png'),
-  );
-  sizes[DangerLevel.Moderate] = Image.resolveAssetSource(
-    require('@app/images/danger-icons/2.png'),
-  );
-  sizes[DangerLevel.Considerable] = Image.resolveAssetSource(
-    require('@app/images/danger-icons/3.png'),
-  );
-  sizes[DangerLevel.Extreme] = Image.resolveAssetSource(
-    require('@app/images/danger-icons/4.png'),
-  );
+  const sizes: Record<DangerLevel, size> = {
+    [DangerLevel.GeneralInformation]: Image.resolveAssetSource(
+      require('@app/images/danger-icons/0.png'),
+    ),
+    [DangerLevel.None]: Image.resolveAssetSource(
+      require('@app/images/danger-icons/0.png'),
+    ),
+    [DangerLevel.Low]: Image.resolveAssetSource(
+      require('@app/images/danger-icons/1.png'),
+    ),
+    [DangerLevel.Moderate]: Image.resolveAssetSource(
+      require('@app/images/danger-icons/2.png'),
+    ),
+    [DangerLevel.Considerable]: Image.resolveAssetSource(
+      require('@app/images/danger-icons/3.png'),
+    ),
+    [DangerLevel.High]: Image.resolveAssetSource(
+      require('@app/images/danger-icons/4.png'),
+    ),
+    [DangerLevel.Extreme]: Image.resolveAssetSource(
+      require('@app/images/danger-icons/5.png'),
+    ),
+  };
 
-  const images: Record<DangerLevel, {(s: ImageStyle): ReactElement}> = {};
-  images[DangerLevel.GeneralInformation] = (s: ImageStyle) => {
-    return (
-      <Image style={s} source={require('@app/images/danger-icons/0.png')} />
-    );
-  };
-  images[DangerLevel.None] = (s: ImageStyle) => {
-    return (
-      <Image style={s} source={require('@app/images/danger-icons/0.png')} />
-    );
-  };
-  images[DangerLevel.Low] = (s: ImageStyle) => {
-    return (
-      <Image style={s} source={require('@app/images/danger-icons/1.png')} />
-    );
-  };
-  images[DangerLevel.Moderate] = (s: ImageStyle) => {
-    return (
-      <Image style={s} source={require('@app/images/danger-icons/2.png')} />
-    );
-  };
-  images[DangerLevel.Considerable] = (s: ImageStyle) => {
-    return (
-      <Image style={s} source={require('@app/images/danger-icons/3.png')} />
-    );
-  };
-  images[DangerLevel.Extreme] = (s: ImageStyle) => {
-    return (
-      <Image style={s} source={require('@app/images/danger-icons/4.png')} />
-    );
+  const images: Record<DangerLevel, {(s: ImageStyle): ReactElement}> = {
+    [DangerLevel.GeneralInformation]: (s: ImageStyle) => {
+      return (
+        <Image style={s} source={require('@app/images/danger-icons/0.png')} />
+      );
+    },
+    [DangerLevel.None]: (s: ImageStyle) => {
+      return (
+        <Image style={s} source={require('@app/images/danger-icons/0.png')} />
+      );
+    },
+    [DangerLevel.Low]: (s: ImageStyle) => {
+      return (
+        <Image style={s} source={require('@app/images/danger-icons/1.png')} />
+      );
+    },
+    [DangerLevel.Moderate]: (s: ImageStyle) => {
+      return (
+        <Image style={s} source={require('@app/images/danger-icons/2.png')} />
+      );
+    },
+    [DangerLevel.Considerable]: (s: ImageStyle) => {
+      return (
+        <Image style={s} source={require('@app/images/danger-icons/3.png')} />
+      );
+    },
+    [DangerLevel.High]: (s: ImageStyle) => {
+      return (
+        <Image style={s} source={require('@app/images/danger-icons/4.png')} />
+      );
+    },
+    [DangerLevel.Extreme]: (s: ImageStyle) => {
+      return (
+        <Image style={s} source={require('@app/images/danger-icons/5.png')} />
+      );
+    },
   };
   let actualStyle: ImageStyle = {...style};
   actualStyle.width = undefined;
